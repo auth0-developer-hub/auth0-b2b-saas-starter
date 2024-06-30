6f6a35aea81f2cd5b4287f526b8f61021a508ef1
guabu <135956181+guabu@users.noreply.github.com>
2024-06-30 11:20:48 +0200

    feat: allow users to delete their accounts

--- app/dashboard/account/profile/actions.ts
+++ app/dashboard/account/profile/actions.ts
@@ -48,3 +48,43 @@ export async function updateDisplayName(formData: FormData) {
 
   return {}
 }
+
+export async function deleteAccount() {
+  const session = await appClient.getSession()
+
+  if (!session) {
+    return redirect("/api/auth/login")
+  }
+
+  try {
+    const { data: members } = await managementClient.organizations.getMembers({
+      id: session!.user.org_id,
+      fields: ["roles"].join(","),
+      include_fields: true,
+    })
+
+    // ensure the user is not the only admin in the organization
+    const admins = members.filter(
+      (m) =>
+        m.roles && m.roles.some((r) => r.id === process.env.AUTH0_ADMIN_ROLE_ID)
+    )
+
+    if (admins.length === 1) {
+      return {
+        error:
+          "You are the only admin in the organization. You cannot delete your account.",
+      }
+    }
+
+    await managementClient.users.delete({
+      id: session.user.sub,
+    })
+
+    return {}
+  } catch (error) {
+    console.error("failed to delete account", error)
+    return {
+      error: "Failed to delete your account.",
+    }
+  }
+}
---
+++ app/dashboard/account/profile/delete-account-form.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { SubmitButton } from "@/components/submit-button"
+
+import { deleteAccount } from "./actions"
+
+export function DeleteAccountForm() {
+  const router = useRouter()
+
+  return (
+    <Card>
+      <form
+        action={async () => {
+          const { error } = await deleteAccount()
+
+          if (error) {
+            toast.error(error)
+          } else {
+            toast.success(
+              "Your account has been deleted and you will be logged out."
+            )
+
+            router.push("/api/auth/logout")
+          }
+        }}
+      >
+        <CardHeader>
+          <CardTitle>Delete Account</CardTitle>
+          <CardDescription>
+            Deleting your account will permanently remove all your data and
+            content. This action cannot be undone.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="flex justify-end">
+          <SubmitButton variant="destructive">Delete Account</SubmitButton>
+        </CardFooter>
+      </form>
+    </Card>
+  )
+}
