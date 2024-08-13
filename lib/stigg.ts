6fd8afd4a7b4ce479d43411f74f6c51d6506d837
Anton Zagrebelny <anton@stigg.io>
2024-08-14 00:46:40 +0300

    fix: mute stigg client during build

--- lib/stigg.ts
+++ lib/stigg.ts
@@ -1,7 +1,18 @@
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_SERVER,
+} from "next/constants"
 import { Stigg } from "@stigg/node-server-sdk"
+
+// Don't open websockets/send requests during build
+const isServerPhase =
+  process.env.NEXT_PHASE === PHASE_PRODUCTION_SERVER ||
+  process.env.NEXT_PHASE === PHASE_DEVELOPMENT_SERVER
 
 export const stiggClient = Stigg.initialize({
   apiKey: process.env.STIGG_SERVER_API_KEY,
+  realtimeUpdatesEnabled: isServerPhase,
+  enableRemoteConfig: isServerPhase,
 })
 
 export async function waitForStiggInit() {
